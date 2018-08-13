c0f20e92c6c5aa11d2999bc1bce100f0ddd1e677
DANCAAA <josecastilloc93@hotmail.com>
2018-08-13 11:19:13 -0500

    FirstCommit

---
+++ 05-Physic Ecuations/Assets/Fermat.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fermat : MonoBehaviour
+{
+    [SerializeField]
+    public float InitialRadious;
+
+    [SerializeField]
+    public float InitialAngu;
+
+
+    [SerializeField]
+    public float w;
+
+    [SerializeField]
+    public float vy;
+
+    [SerializeField]
+    public float GrowVelocity;
+
+    float t = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float r = t * t;
+        t += Time.deltaTime;
+        float x = r * Mathf.Cos(w * t);
+        float z = r * Mathf.Sin(w * t);
+        float y = t;
+        Vector3 pos = new Vector3(x, y, z);
+        transform.localPosition = pos;
+    }
+}
---
+++ 05-Physic Ecuations/Assets/GravityForce.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityForce : MonoBehaviour
+{
+    [SerializeField]
+    float Factor;
+    public Transform target;
+ 
+
+    // Use this for initialization
+    void Start ()
+    {
+        double G = 6.67408 * Factor;
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 TargetDirection = target.position - transform.position;
+        float angle = Vector3.Angle(TargetDirection, transform.forward);
+        if (angle > 0.01f)
+        {
+            print("Excuting gravity atraction");
+            
+        }
+    }
+
+    void FixedUpdate()
+    {   
+    }
+}
