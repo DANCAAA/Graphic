1c95911afaec91d79469f192120c98d9484f4e9f
Daniel Cano <josecastilloc93@gmail.com>
2018-08-28 10:52:05 -0500

    Elemental Attack
    Elemental

---
+++ 06-AnimationSyncronitationSpell/Assets/LightControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControler : MonoBehaviour
+{
+    [SerializeField]
+    Gradient colorGradient;
+
+    float t = 0;
+    Light mLight;
+    ParticleSystem mParticleSys;
+    ParticleSystem.MainModule mParticleMain;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mLight = GetComponent<Light>();
+        mParticleSys = GetComponentInChildren<ParticleSystem>();
+        mParticleMain = mParticleSys.main;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        t += Time.deltaTime;
+        mLight.color = colorGradient.Evaluate(t / 6);
+        mParticleMain.simulationSpeed = 0.5f;
+	}
+}
---
+++ 07-ElementalAttack/Assets/ChangePartycleSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePartycleSystem : MonoBehaviour
+{
+    ParticleSystem ps;
+    public float speed;
+    // Use this for initialization
+    void Start () {
+        ps = GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward*(-1) * Time.deltaTime, Camera.main.transform);
+        var emision = ps.emission;
+        var shape = ps.shape;
+        
+        
+	}
+}
